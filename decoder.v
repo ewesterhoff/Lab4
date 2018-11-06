50b0771b4cf3f7d1677af8fbee02949197f32d23
Adam Selker <adamselker@gmail.com>
2018-11-05 19:57:19 -0500

    Fix decoder...?

--- decoder.v
+++ decoder.v
@@ -1,6 +1,5 @@
 // Decoder.  Built for bleep_vim's CPU, will need tweaks.
 
-
 `define ADD  3'd0
 `define SUB  3'd1
 `define XOR  3'd2
@@ -12,12 +11,10 @@
 
 module decoder(
 	input [31:0] cmd,
-	output immSel, memWrEn, regWrEn,
-	output [1:0] DwSel, jSel, pcSel, 
+	output immSel, memWrEn, regWrEn, DwSel,
 	output [4:0] Aa, Ab, Aw, 
 	output [2:0] aluOp,
 	output [15:0] imm,
-	output [25:0] jumpAddr
 );
 
 	// Always in the same place in the command, though not always there
@@ -25,18 +22,15 @@ module decoder(
 	wire [5:0] funct; assign funct = cmd[5:0];
 
 	assign imm = cmd[15:0];
-	assign jumpAddr = cmd[25:0];
 
 	// One-hot representation of opcodes / functs
 	wire lw; assign lw = (opcode == 6'h23);
 	wire sw; assign sw = (opcode == 6'h2b);
 	wire j; assign j = (opcode == 6'h2);
-	wire jal; assign jal = (opcode == 6'h3);
 	wire beq; assign beq = (opcode == 6'h4);
 	wire bne; assign bne = (opcode == 6'h5);
 	wire xori; assign xori = (opcode == 6'he);
 	wire addi; assign addi = (opcode == 6'h8);
-	wire jr; assign jr = (opcode == 6'h0  && funct == 6'h8);
 	wire add; assign add = (opcode == 6'h0 && funct == 6'h20);
 	wire sub; assign sub = (opcode == 6'h0 && funct == 6'h22);
 	wire slt; assign slt = (opcode == 6'h0 && funct == 6'h2a);
@@ -44,15 +38,13 @@ module decoder(
 	// Addresses of the registers we'll read from and/or write to
 	assign Aa = cmd[25:21];
 	assign Ab = cmd[20:16];
-	assign Aw = jal ? 5'd31 : (lw | addi | xori ? Ab : cmd[15:11]);
+	assign Aw = (lw | addi | xori) ? Ab : cmd[15:11];
 
 	// Selectors for various things
 	assign immSel = lw | sw | addi | xori; // Use an immediate?
 	assign aluOp = xori ? `XOR : (slt ? `SLT : (beq | bne | sub ? `SUB : `ADD)); // What operation in the main ALU?
-	assign DwSel = lw ? 2'd2 : (jal ? 2'd1 : 2'd0); // What data to write to the register file?
-	assign jSel = jr ? 2'd0 : (jal || j ? 2'd1 : 2'd2); // What should the PC be next cycle?
-	assign pcSel = beq ? 2'd1 : (bne ? 2'd2 : 2'd0); // What should we add to the PC (and maybe feed back into it)?
+	assign DwSel = lw; // What data to write to the register file?
 	assign memWrEn = sw; // Write to memeory?
-	assign regWrEn = !(sw | j | beq | bne | jr); // Write to register?
+	assign regWrEn = !(sw | j | beq | bne); // Write to register?
 
 endmodule
