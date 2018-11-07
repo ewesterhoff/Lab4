bea8e452b84113563ec98fb3aadd783bdb168ac9
anniekroo <annie.kroo@students.olin.edu>
2018-11-07 09:32:33 -0500

    adding cpu with instantiation of submods

--- cpu.v
+++ cpu.v
@@ -3,16 +3,21 @@
 `include "dataMemory.v"
 `include "decoder.v"
 `include "regfile.v"
+
 module cpu(
   input clk,
   output dataOut,
   output regDest,
-  output Dw );
-  always @(posedge clk)begin
-  FullAdder32bit adder(.sum(), .carryout(), .overflow(), .zeros(), .a(), .b(), .carryin());
-  ALUcontrolLUT alu( .alu_code0(), .alu_code1(), .alu_code2(), .set_flags(), .slt_enable(), .subtract(), .ALUcommand());
-  module dataMemory( .clk(),.dataOut(), .address(), .writeEnable(), .dataIn());
-  decoder decode( .cmd(), .immSel(), .memWrEn(), .regWrEn(), .DwSel(), .Aa(), .Ab(), .Aw(), .aluOp(), .imm());
-  regfile reg( .ReadData1(), .ReadData2(), .WriteData(), .ReadRegister1(), .ReadRegister2(), .WriteRegister(), .RegWrite(),	.Clk());
-  end
+  output Dw
+  );
+
+    complexPC pc(.clk(), .isJmp(), .isJr(), .isBr(),.imm(), .rrt(),.pcData());
+    FullAdder32bit adder(.sum(), .carryout(), .overflow(), .zeros(), .a(), .b(), .carryin());
+    ALUcontrolLUT alu( .alu_code0(), .alu_code1(), .alu_code2(), .set_flags(), .slt_enable(), .subtract(), .ALUcommand());
+    dataMemory mem( .clk(),.dataOut(), .address(), .writeEnable(), .dataIn());
+    decoder decode( .cmd(), .immSel(), .memWrEn(), .regWrEn(), .DwSel(), .Aa(), .Ab(), .Aw(), .aluOp(), .imm());
+    regfile reg( .ReadData1(), .ReadData2(), .WriteData(), .ReadRegister1(), .ReadRegister2(), .WriteRegister(), .RegWrite(),	.Clk());
+
+
+
 endmodule
