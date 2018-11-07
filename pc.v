9da1fc1163d3a1f90e8b35de92cff7a8059dd1cb
Adam Selker <adamselker@gmail.com>
2018-11-07 09:30:34 -0500

    Add PC

---
+++ pc.v
@@ -0,0 +1,30 @@
+// A PC with no flow control.
+module linearPC(
+	input clk,
+	output pcData
+);
+
+	reg [31:0] pc;
+	initial pc <= 0;
+	always @(posedge clk) pc <= pc + 32'd4;
+	assign pcData = pc;
+
+endmodule
+
+
+// A real PC that can do flow control
+module complexPC(
+	input clk, isJmp, isJr, isBr,
+	input imm, rrt,
+	output pcData
+);
+	
+	reg [31:0] pc;
+	initial pc <= 0;
+	always @(posedge clk) begin
+		if (isJmp) pc <= {pc[31:28], imm, 2'b0};
+		if (isJr) pc <= rrt;
+		if (isBr) pc <= pc + 32'd4 + {14'b0, imm, 2'b0};
+	end
+
+endmodule
