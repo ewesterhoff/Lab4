e3ec266e56f0249aecb44f9da041ca33553d24ff
ewesterhoff <emma.westerhoff@students.olin.edu>
2018-11-07 09:25:15 -0500

    dual instruction-data memory

--- dataMemory.v
+++ dataMemory.v
@@ -3,6 +3,7 @@
 // Three ports: dataIn (write), dataOut (read), cmdOut (read)
 // dataIn and dataOut share an address
 
+/*
 module dataMemory
 (
   input clk, regWE, // On rising edge, iff regWE, writes data to register
@@ -25,4 +26,49 @@ module dataMemory
 
   assign dataOut = mem[dataAddr]; // Data output
   assign cmdOut = mem[cmdAddr]; // Command output
+endmodule
+
+*/
+// ------------------------------------------------------------------------
+// Data Memory
+//   Positive edge triggered
+//   dataOut always has the value mem[address]
+//   If writeEnable is true, writes dataIn to mem[address]
+//   March and Emma
+// ------------------------------------------------------------------------
+
+// Text menory size: 1024
+// Datapath memory size: 4096-1024 = 3072
+module dataMemory
+#(
+    parameter depth = 32'b00000000000000000000010000000000,
+    parameter offset = 32'b000000000000000000000000000000
+)
+(
+    input                     clk,
+    output[31:0]            dataOut,
+    input [31:0]            address,
+    input               writeEnable,
+    input [31:0]             dataIn
+);
+    // convert address from mips thing to verilog index 0,4,8 to 0,1,2
+    // 0x1000 to incoming address. add offset parameter
+
+    reg [31:0] memory [depth-1:0];
+    wire[31:0] shift_address; // use the ALU to shift the address as necessary (shift if accessing datamemory)
+    wire z, c, o; // for alu
+
+    ALU alu(.result(shift_address), .carryout(c), .zero(z), .overflow(o),
+  .operandA(address), .operandB(offset), .command(3'd1)); //sub
+
+    wire [31:0] div4_address = shift_address >> 2;
+
+    assign dataOut = memory[div4_address];
+
+    always @(posedge clk) begin
+        if(writeEnable)begin
+            memory[div4_address] <= dataIn;
+            end
+    end
+
 endmodule
