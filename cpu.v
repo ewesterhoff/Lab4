2a51f34c13343ecdba8a6e8478b720fea02194f1
anniekroo <annie.kroo@students.olin.edu>
2018-11-07 21:13:47 -0500

    adding cpu with flipflops

--- cpu.v
+++ cpu.v
@@ -15,15 +15,18 @@ module cpu(
   reg[31:0] rrtOrImm1, rrtOrImm2;
   reg[31:0] imm1, imm2, imm3;
   reg[31:0] aluOut2, aluOut3, aluOut4;
-  s
+  reg[31:0] dw3, dw4;
+  reg[31:0] addrReg1, addrReg2, addrReg3, addrReg4;
 
 
-    always @(posedge clk) cmd1 = cmd0;
+    always @(posedge clk)begin cmd1 = cmd0; end
     complexPC pc(.clk(clk), .isJmp(), .isJr(), .isBr(),.imm(), .rrt(),.pcData());
-    FullAdder32bit adder(.sum(), .carryout(), .overflow(), .zeros(), .a(), .b(), .carryin());
-    ALUcontrolLUT alu( .alu_code0(), .alu_code1(), .alu_code2(), .set_flags(), .slt_enable(), .subtract(), .ALUcommand());
-    dataMemory mem( .clk(clk),.dataOut(), .instruction(), .address(), .pc_address(), .writeEnable(), .dataIn());
     decoder decode( .cmd(), .immSel(), .memWrEn(), .regWrEn(), .isJmp(), .isJr(), .DwSel(), .brSel(), .Aa(), .Ab(), .Aw(), .aluOp(), .imm(), .jumpAddr());
     regfile register( .ReadData1(), .ReadData2(), .WriteData(), .ReadRegister1(), .ReadRegister2(), .WriteRegister(), .RegWrite(),	.Clk(clk));
+    always @(posedge clk)begin rrs2 = rrs1; rrtOrImm2 = rrtOrImm1; imm2 = imm1; addrReg2 = addrReg1; end
+    ALUcontrolLUT alu( .alu_code0(), .alu_code1(), .alu_code2(), .set_flags(), .slt_enable(), .subtract(), .ALUcommand());
+    always @(posedge clk)begin rrs3 = rrs2; imm3 = imm2; addrReg3 = addrReg2; aluOut3 = aluOut2; end
+    dataMemory mem( .clk(clk),.dataOut(), .instruction(), .address(), .pc_address(), .writeEnable(), .dataIn());
+    always @(posedge clk)begin addrReg4 = addrReg3; aluOut4 = aluOut3; end
 
 endmodule
