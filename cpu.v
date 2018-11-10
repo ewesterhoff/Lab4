8d5fa66d47d6c8d7da93cf4bbef67aca10502cbc
anniekroo <annie.kroo@students.olin.edu>
2018-11-10 16:56:59 -0500

    sending rrt through pipline

--- cpu.v
+++ cpu.v
@@ -49,11 +49,11 @@ module cpu(
 	regfile register( .ReadData1(rrs1), .ReadData2(rrt1), .WriteData(Dw4), .ReadRegister1(rs), .ReadRegister2(rt), .WriteRegister(addrReg4), .RegWrite(regWrEn4),	.Clk(clk));
 	assign rrtOrImm1 = immSel1 ? imm1 : rrt1;
 
-	always @(posedge clk)begin rrs2 <= rrs1; regWrEn2 = regWrEn1; rrtOrImm2 <= rrtOrImm1; imm2 <= imm1; addrReg2 <= addrReg1; aluOp2 <= aluOp1; cmd2 <= cmd1; end
+	always @(posedge clk)begin rrs2 <= rrs1; regWrEn2 <= regWrEn1; rrtOrImm2 <= rrtOrImm1; imm2 <= imm1; addrReg2 <= addrReg1; aluOp2 <= aluOp1; cmd2 <= cmd1; rrt2 <= rrt1; end
 
 	alu math(.result(aluOut2),.carryout(carryout),.zero(zero),.overflow(overflow), .operandA(rrs2), .operandB(rrtOrImm2), .command(aluOp2));
 
-	always @(posedge clk)begin rrs3 <= rrs2; regWrEn3 = regWrEn2; imm3 <= imm2; addrReg3 <= addrReg2; aluOut3 <= aluOut2; cmd3 <= cmd2; end
+	always @(posedge clk)begin rrs3 <= rrs2; regWrEn3 <= regWrEn2; imm3 <= imm2; addrReg3 <= addrReg2; aluOut3 <= aluOut2; cmd3 <= cmd2; rrt3 <= rrt2; end
 
 	dataMemory mem( .clk(clk),.dataOut(memOut), .instruction(cmdDecodelet), .address(aluOut3), .pc_address(pc), .writeEnable(memWrEn3), .dataIn(rrt3));
 	assign Dw3 = (DwSel3 == 2'd2) ? memOut : aluOut3;
