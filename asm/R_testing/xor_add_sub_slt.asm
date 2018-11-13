6d2ada103e81ed69f79f02b18d9497efe5e7da2c
Adam Selker <adamselker@gmail.com>
2018-11-12 19:11:07 -0500

    Add nop's do avoid data hazard

--- asm/R_testing/xor_add_sub_slt.asm
+++ asm/R_testing/xor_add_sub_slt.asm
@@ -3,6 +3,11 @@ li $sp 0x3ffc
 addi	$t0, $zero, 21		# i, the current array element being accessed
 addi	$t1, $zero, 30	
 
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+add $zero, $zero, $zero
+
 add $t3, $t0, $t1	#51
 sub $t4, $t0, $t1	#-9
 
