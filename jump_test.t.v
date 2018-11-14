cdbe5066744a25fa4bd7cfb8df4c14176eb856fb
ewesterhoff <emma.westerhoff@students.olin.edu>
2018-11-14 10:24:16 -0500

    add jump test

---
+++ jump_test.t.v
@@ -0,0 +1,24 @@
+`include "cpu.v"
+
+module jump_test ();
+	reg clk;
+
+	initial clk = 0;
+	always #10 clk =! clk;
+
+	cpu dut(.clk(clk));
+
+    initial begin
+
+    	$dumpfile("asm/Flow_Control/jump_waves.vcd");
+    	$dumpvars();
+
+    	$readmemh("asm/Flow_Control/jump.text.hex", dut.mem.memory, 0);
+
+
+    	#300
+    	$finish();
+    end
+
+
+endmodule
