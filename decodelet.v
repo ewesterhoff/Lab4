c5ddb33e4aa3678ff6d4e572de104ff3c116e8b0
Adam Selker <adamselker@gmail.com>
2018-11-14 09:24:08 -0500

    actually bubble

--- decodelet.v
+++ decodelet.v
@@ -10,17 +10,18 @@ module decodelet(
 	wire [5:0] opcode; assign opcode = cmdIn[31:26];
 	wire [5:0] funct; assign funct = cmdIn[5:0];
 
-	wire lw; assign lw = (opcode == 6'h23);
 	wire j; assign j = (opcode == 6'h2);
 	wire jr; assign jr = (opcode == 6'h0  && funct == 6'h8);
 	wire beq; assign beq = (opcode == 6'h4);
 	wire bne; assign bne = (opcode == 6'h5);
 
-	assign isBubble = lw;
-	assign isJmp = !lw && ( j | jr);
+	wire lastLw; assign lastLw = (lastCmdIn[31:26] == 6'h23);
+
+	assign isBubble = lastLw;
+	assign isJmp = !lastLw && ( j | jr);
 	assign isJr = 0; // No jump reg yet
 	assign isBr = 0; // No bromine yet 
 
-	assign cmdOut = cmdIn;
+	assign cmdOut = lastLw ? 32'h00000020 : cmdIn; // Send " add $zero, $zero, $zero " for bubbles
 
 endmodule
